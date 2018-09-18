7c686f79c7308359fc6b895ea9ae44e552a12e83
Damien Picard <dam.pic@free.fr>
2018-09-18 20:07:43 +0200

    Zoom

--- Graph.gd
+++ Graph.gd
@@ -3,10 +3,10 @@ extends Control
 export(NodePath) var sequences_container
 var polyline
 var display_polyline
-var graph_location
-var graph_scale = Vector2(1, -1)
+var graph_transform = Transform2D()
 var zoom_speed = 1.1
 var zoom_start
+var zoom_start_transform = Transform2D()
 
 var is_dragging = false
 var is_zooming = false
@@ -24,23 +24,20 @@ func add_sequence(polyline, name, min_size, max_size, min_frame, max_frame):
 
 func _ready():
     add_sequence([Vector2(0, 0), Vector2(50, 100), Vector2(60, 100)], "truc", 0, 100, 0, 60)
-    graph_location = rect_size / 2
+    graph_transform[2] = rect_size / 2
     update_graph()
     update()
 
 func update_graph():
-#    var transform = Transform2D().scaled(graph_scale)
-#    for i in range(len(polyline)):
-#        display_polyline[i] = transform * polyline[i] + graph_location
-    var transform = Transform2D().scaled(graph_scale).translated(Vector2(graph_location.x, 0.0)).translated(Vector2(0.0, -graph_location.y))
-    print(transform)
     for i in range(len(polyline)):
-        display_polyline[i] = transform.xform(polyline[i])
+        display_polyline[i] = graph_transform.xform(polyline[i])
 
-func zoom_graph(center, factor):
-    print(center)
-    graph_scale *= factor
-#    graph_location. += 
+func zoom_graph(center, factor, base_transform=graph_transform):
+    var transform = Transform2D()
+    transform.origin -= center
+    transform = transform.scaled(factor)
+    transform.origin += center
+    graph_transform = transform * base_transform
     update_graph()
     update()
     
@@ -52,9 +49,19 @@ func _gui_input(event):
                 # Activate zoom
                 is_zooming = event.pressed
                 zoom_start = event.position
+                zoom_start_transform = graph_transform
+                if is_zooming:
+                    mouse_default_cursor_shape = Control.CURSOR_DRAG
+                else:
+                    mouse_default_cursor_shape = Control.CURSOR_ARROW
             else:
                 # Activate drag
                 is_dragging = event.pressed
+                if is_dragging:
+                    mouse_default_cursor_shape = Control.CURSOR_DRAG
+                else:
+                    mouse_default_cursor_shape = Control.CURSOR_ARROW
+                update()
         # Zoom
         elif event.button_index == BUTTON_WHEEL_UP:
             zoom_graph(event.position, Vector2(zoom_speed, zoom_speed))
@@ -63,22 +70,38 @@ func _gui_input(event):
     # Drag
     if event is InputEventMouseMotion:
         if is_dragging:
-            graph_location += event.relative
+            graph_transform[2] += event.relative
             update_graph()
             self.update()
         elif is_zooming:
-            zoom_graph(event.position - zoom_start)
+            print(mouse_default_cursor_shape)
+            var factor = (event.position - zoom_start)
+            factor.x = pow(1.01, factor.x)
+            factor.y = pow(1.01, -factor.y)
+            zoom_graph(rect_size / 2,
+                       factor,
+                       zoom_start_transform)
             update_graph()
             self.update()
 
 func draw_axes():
-    pass
+    var size = graph_transform * rect_size
+    var x = size.x
+    var y = size.y
+    
+    while x < size.x:
+        draw_line(Vector2(graph_transform.origin.x, 0.0),
+                  Vector2(graph_transform.origin.x, rect_size.y),
+                  Color(1.0, 1.0, 1.0, 0.1), 1.0, true)
+        x += 15
+#    while y < size.y:
+#        draw_line(Vector2(0.0, y), Vector2(rect_size.x, y), Color(1.0, 1.0, 1.0, 0.1), 1.0, true)
+#        y += 15
     
 func _draw():
-#    draw_set_transform(graph_location, 0.0, graph_scale)
+    draw_axes()
     for point in display_polyline:
         draw_circle(point, 3, Color(1,0,0) )
     
-    draw_axes()
 
     draw_polyline(display_polyline, Color(1.0, 1.0, 1.0, 0.5), 1.0, true)
