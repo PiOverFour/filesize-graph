762ac6dfa3977814687a8f1eb7468c41048ff521
Damien Picard <dam.pic@free.fr>
2018-09-28 00:30:36 +0200

    Image panel custom layout

--- ImageContainer.gd
+++ ImageContainer.gd
@@ -1,7 +1,14 @@
-extends GridContainer
+extends Container
 
+export(int) var image_size = 20
+export(int) var margin_min = 2
 onready var main_node = get_node("../../..")
 
+var row_offset = 0
+var previous_height = -1
+
+func _ready():
+    _on_ImageContainer_sort_children()
 
 func draw_highlight():
     if main_node.do_highlight:
@@ -13,3 +20,29 @@ func draw_highlight():
 
 func _draw():
     draw_highlight()
+
+
+func _on_ImageContainer_sort_children():
+    # Calculate margin
+    var scrollbar = $"../VScrollBar"
+
+    var width = rect_size.x
+    var square_size = image_size + margin_min
+    var row_squares = floor(width / square_size)
+    var margin = (width - (row_squares*image_size)) / (row_squares-1)
+    square_size = image_size + margin
+    # Layout images
+    for i in range(get_child_count()):
+        fit_child_in_rect(get_child(i), Rect2(square_size * fmod(i, row_squares),
+                                              square_size * (floor(i / row_squares)-row_offset),
+                                              image_size, image_size))
+    # Update Scrollbar
+    if get_child_count() and previous_height != rect_size.y:
+        var column_squares = ceil(get_child_count() / row_squares)
+        var page = ceil(rect_size.y / square_size)
+        scrollbar.max_value = floor(column_squares) + 1
+        scrollbar.page = min(page, scrollbar.max_value)
+
+func _on_VScrollBar_value_changed(value):
+    row_offset = value
+    _on_ImageContainer_sort_children()
